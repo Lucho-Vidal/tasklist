6ad9f067cc913029d5741e8abde71bb3f97c45c3
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2023-11-29 20:58:52 -0300

    created project

---
+++ client/src/components/TaskCard.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+
+export function TaskCard({ task }) {
+    const navigate = useNavigate();
+    return (
+        <div
+            className="bg-zinc-800 p-3 hover:bg-zinc-700"
+            onClick={() => {
+                navigate(`/tasks/${task.id}`);
+            }}
+        >
+            <h1 className="font-bold uppercase">{task.title}</h1>
+            <p className="text-slate-400">{task.description}</p>
+        </div>
+    );
+}
+
+//solución consultada a chatGPT
+TaskCard.propTypes = {
+    task: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+    }).isRequired,
+};
